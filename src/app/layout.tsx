5c25e5861e4309c4b9c87522d39417baaa9062ea
wesleychapman04 <wesleyachapman@outlook.com>
2023-10-25 17:09:58 +1100

    Inter Add md editor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,16 @@
+import '@radix-ui/themes/styles.css'
+import './theme-config.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Nav from './Nav'
-import '@radix-ui/themes/styles.css'
 import { Theme } from '@radix-ui/themes'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-inter',
+})
 
 export const metadata: Metadata = {
     title: 'Create Next App',
@@ -19,10 +24,9 @@ export default function RootLayout({
 }) {
     return (
         <html lang='en'>
-            <body className={inter.className}>
-                <Theme>
+            <body className={inter.variable}>
+                <Theme appearance='light' accentColor='violet'>
                     <Nav />
-
                     <main className='p-5'>{children}</main>
                 </Theme>
             </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <>
-      <h1>Hi</h1>
-    </>
-  )
+    return (
+        <>
+            <h1>Hi</h1>
+        </>
+    )
 }
