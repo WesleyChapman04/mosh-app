63674b553067ac23733998c60405c974692c87e0
wesleychapman04 <wesleyachapman@outlook.com>
2023-10-27 13:40:10 +1100

    API For Deleting Issues

--- src/app/issues/[id]/EditIssueButton.tsx
+++ src/app/issues/[id]/EditIssueButton.tsx
@@ -4,9 +4,11 @@ import Link from 'next/link'
 
 export default function EditIssueButton({ issueId }: { issueId: number }) {
     return (
-        <Button>
-            <Pencil2Icon />
-            <Link href={`/issues/${issueId}/edit`}>Edit Issue</Link>
-        </Button>
+        <Link href={`/issues/${issueId}/edit`} className='max-w-full'>
+            <Button className='w-full'>
+                <Pencil2Icon />
+                Edit Issue
+            </Button>
+        </Link>
     )
 }
