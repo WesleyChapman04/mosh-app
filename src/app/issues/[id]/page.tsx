cedb09e13659f1974534877788406c72310e628c
wesleychapman04 <wesleyachapman@outlook.com>
2023-10-27 13:25:31 +1100

    HElp

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -1,35 +1,34 @@
-import { Box, Grid } from '@radix-ui/themes'
+import { Box, Flex, Grid } from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
 import prisma from '../../../../prisma/client'
+import DeleteIssueButton from './DeleteIssueButton'
 import EditIssueButton from './EditIssueButton'
 import IssueDetails from './IssueDetails'
-import DeleteIssueButton from './DeleteIssueButton'
 
 interface Props {
     params: { id: string }
 }
 
-export default async function DetailPage({ params }: Props) {
+const IssueDetailPage = async ({ params }: Props) => {
     const issue = await prisma.issue.findUnique({
         where: { id: parseInt(params.id) },
     })
 
-    if (!issue) {
-        notFound()
-    }
+    if (!issue) notFound()
+
     return (
-        <Grid columns={{ initial: '1', sm: '5' }} gap={'5'}>
+        <Grid columns={{ initial: '1', sm: '5' }} gap='5'>
             <Box className='md:col-span-4'>
                 <IssueDetails issue={issue} />
             </Box>
             <Box>
-                <div className='flex flex-col gap-4'>
+                <Flex direction='column' gap='4'>
                     <EditIssueButton issueId={issue.id} />
                     <DeleteIssueButton issueId={issue.id} />
-                </div>
+                </Flex>
             </Box>
         </Grid>
     )
 }
 
-const dynamic = 'force-dynamic'
+export default IssueDetailPage
