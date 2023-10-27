00ca122bca5404df343f08097aabc5539d78f1f1
wesleychapman04 <wesleyachapman@outlook.com>
2023-10-27 11:18:11 +1100

    Fixing Loading Experience

--- src/app/issues/[id]/edit/page.tsx
+++ src/app/issues/[id]/edit/page.tsx
@@ -1,10 +1,9 @@
 import dynamic from 'next/dynamic'
 import { notFound } from 'next/navigation'
 import prisma from '../../../../../prisma/client'
-import IssueForm from '../../_components/IssueForm'
 import IssueFormSkeleton from './loading'
 
-const issueForm = dynamic(() => import('@/app/issues/_components/IssueForm'), {
+const IssueForm = dynamic(() => import('@/app/issues/_components/IssueForm'), {
     ssr: false,
     loading: () => <IssueFormSkeleton />,
 })
--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,8 +1,7 @@
 import dynamic from 'next/dynamic'
-import IssueForm from '../_components/IssueForm'
 import IssueFormSkeleton from './loading'
 
-const issueForm = dynamic(() => import('@/app/issues/_components/IssueForm'), {
+const IssueForm = dynamic(() => import('@/app/issues/_components/IssueForm'), {
     ssr: false,
     loading: () => <IssueFormSkeleton />,
 })
