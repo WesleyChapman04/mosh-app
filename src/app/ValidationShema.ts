d58889961bc2bab2faec7bd8d59fa1ae851dea05
wesleychapman04 <wesleyachapman@outlook.com>
2023-10-30 15:57:43 +1100

    Api now assigns issues

--- src/app/ValidationShema.ts
+++ src/app/ValidationShema.ts
@@ -1,6 +1,13 @@
 import { z } from 'zod';
 
 export const IssueSchema = z.object({
-    title: z.string().min(1).max(255),
-    description: z.string().min(1)
+    title: z.string().min(1, 'Title is Required').max(255),
+    description: z.string().min(1, 'Description is Required').max(65535)
 });
+
+export const PacthIssueSchema = z.object({
+    title: z.string().min(1, 'Title is Required').max(255).optional(),
+    description: z.string().min(1, 'Description is Required').max(65535).optional(),
+    assignedToUserId: z.string().min(1, 'Assinged To User Id is Required').max(255).optional().nullable(),
+});
+
--- src/app/api/issues/[id]/route.ts
+++ src/app/api/issues/[id]/route.ts
@@ -1,4 +1,4 @@
-import { IssueSchema } from "@/app/ValidationShema";
+import { IssueSchema, PacthIssueSchema } from "@/app/ValidationShema";
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "../../../../../prisma/client";
 import { AuthOptions } from "@/app/auth/AuthOptions";
@@ -6,14 +6,25 @@ import { getServerSession } from "next-auth";
 
 
 export async function PATCH(request: NextRequest, { params}: { params: { id: string}}) {
-    const session = await getServerSession(AuthOptions)
-    if (!session) {
-        return NextResponse.json({ error: 'Unauthorized'}, { status: 401 })
-    }
+    // const session = await getServerSession(AuthOptions)
+    // if (!session) {
+    //     return NextResponse.json({ error: 'Unauthorized'}, { status: 401 })
+    // }
     const body = await request.json()
-    const validation = IssueSchema.safeParse(body)
+    const validation = PacthIssueSchema.safeParse(body)
     if (!validation.success) {
         return NextResponse.json(validation.error.format(), { status: 400 })
+    }
+
+    const {assignedToUserId, title, description} = body
+
+    if (assignedToUserId) {
+        const user = await prisma.user.findUnique({
+            where: { id: assignedToUserId}
+        })
+        if (!user) {
+            return NextResponse.json({ error: 'Invalid User'}, { status: 400 })
+        }
     }
 
     const issue = await prisma.issue.findUnique({
@@ -25,7 +36,7 @@ export async function PATCH(request: NextRequest, { params}: { params: { id: str
     
     const updatedIssue = await prisma.issue.update({
         where: { id: issue.id },
-        data: { title: body.title, description: body.description}
+        data: { title, description, assignedToUserId}
     })
 
     return NextResponse.json(updatedIssue)
